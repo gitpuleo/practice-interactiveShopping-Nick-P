756ee5282a5a242dab5b91720ea9aa5fa7cec972
nfp <nfpuleo@gmail.com>
2025-06-10 20:43:53 -0500

    staging

---
+++ logic.js
@@ -0,0 +1,14 @@
+const addBtn = document.getElementById('add-button');
+const input = document.getElementById('list-items');
+const list = document.getElementById('shopping-list');
+
+
+
+function addListItem() {
+    const li = document.createElement('li');
+
+
+
+    
+}
+
