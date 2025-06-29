57b13887dca8970b8125c2f5eaa3ab5a3b7789a1
nfp <nfpuleo@gmail.com>
2025-06-28 20:51:06 -0500

    All tasks but edit button complete

--- logic.js
+++ logic.js
@@ -3,12 +3,27 @@ const input = document.getElementById('list-items');
 const list = document.getElementById('shopping-list');
 
 
+addBtn.addEventListener("click", addListItem);
+
+
+
+
 
 function addListItem() {
     const li = document.createElement('li');
+    const removeButton = document.createElement('button');
+   
+    li.textContent = input.value;
+    removeButton.textContent = "Delete";
+    removeButton.addEventListener("click", () => li.remove());
+    li.appendChild(removeButton);
+    list.appendChild(li);
+    input.value = "";
 
 
-
-    
+    const editButton = document.createElement('button');
+    editButton.textContent = "Edit";
+   // editButton.addEventListener('click', () => li.textContent = )
 }
 
+//Stumped on implementing the logic for the edit button
